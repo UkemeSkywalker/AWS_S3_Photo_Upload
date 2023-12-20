3f68b35495601b00d077cb8656e7851434b55b95
Ukeme David Eseme <skywalkershadow9@gmail.com>
2023-12-20 21:04:39 +0000

    Revert "created UI components"
    This reverts commit ad36b8e96272a1d0221c487efceb4432ca2ce14d.
    
    correct

--- frontend/src/ui/components/search-bar.js
+++
@@ -1,9 +0,0 @@
-const Searchbar = () => {
-    return(
-        <div className="search pt-5 ">
-              <input className="h-10 w-full rounded-full px-5" type="text" id="search-input"  placeholder="Search..." />
-            </div>
-    )
-}
-
-export default Searchbar
