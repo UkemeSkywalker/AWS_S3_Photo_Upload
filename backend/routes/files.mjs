29f9d5332376a964709d640da3c00078226b3677
Ukeme David Eseme <skywalkershadow9@gmail.com>
2023-12-20 11:42:56 +0000

    routes

---
+++ backend/routes/files.mjs
@@ -0,0 +1,83 @@
+import { Router } from "express";
+
+export const router = Router()
+
+
+const files = [
+    {
+      "fileName": "report.pdf",
+      "fileType": "document", 
+      "fileSize": "2.1 MB",
+      "downloadUrl": "https://example.com/files/report.pdf"
+    },
+    {
+      "fileName": "presentation.pptx",
+      "fileType": "document",
+      "fileSize": "8.4 MB", 
+      "downloadUrl": "https://example.com/files/presentation.pptx"
+    },
+    {
+      "fileName": "song.mp3",
+      "fileType": "audio",
+      "fileSize": "4.5 MB",
+      "downloadUrl": "https://example.com/files/song.mp3"  
+    },
+    {
+      "fileName": "tutorial.mp4",
+      "fileType": "video",
+      "fileSize": "124 MB",
+      "downloadUrl": "https://example.com/files/tutorial.mp4"
+    },
+    {
+      "fileName": "report2.pdf",
+      "fileType": "document",
+      "fileSize": "1.2 MB",
+      "downloadUrl": "https://example.com/files/report2.pdf"
+    },
+    {
+      "fileName": "presentation2.pptx", 
+      "fileType": "document",
+      "fileSize": "6.7 MB",
+      "downloadUrl": "https://example.com/files/presentation2.pptx"
+    },
+    {
+      "fileName": "song2.mp3",
+      "fileType": "audio",
+      "fileSize": "3.1 MB",
+      "downloadUrl": "https://example.com/files/song2.mp3"   
+    },
+    {
+      "fileName": "video.avi", 
+      "fileType": "video",
+      "fileSize": "250 MB",
+      "downloadUrl": "https://example.com/files/video.avi"
+    },
+    {
+      "fileName": "ebook.pdf",
+      "fileType": "document",
+      "fileSize": "5 MB",
+      "downloadUrl": "https://example.com/files/ebook.pdf"
+    },
+    {
+      "fileName": "podcast.mp3",
+      "fileType": "audio",
+      "fileSize": "11 MB",
+      "downloadUrl": "https://example.com/files/podcast.mp3"
+    }
+  ]
+
+
+// get all files
+router.get("/", (req, res) => {
+    res.send(files)
+})
+
+
+// upload new file
+router.post("/", (req, res) => {
+    files.push(req.body)
+    res.send(201)
+})
+
+
+
--- backend/src/index.mjs
+++ backend/src/index.mjs
@@ -1,5 +1,8 @@
 import exp from "constants";
 import express from "express"
+import {router} from "../routes/files.mjs"
+
+const path = router
 
 const app = express()
 
@@ -12,79 +15,5 @@ app.listen(PORT, (req, res) => {
 app.use(express.json());
 app.use(express.urlencoded())
 
+app.use("/api/vi", path)
 
-const files = [
-    {
-      "fileName": "report.pdf",
-      "fileType": "document", 
-      "fileSize": "2.1 MB",
-      "downloadUrl": "https://example.com/files/report.pdf"
-    },
-    {
-      "fileName": "presentation.pptx",
-      "fileType": "document",
-      "fileSize": "8.4 MB", 
-      "downloadUrl": "https://example.com/files/presentation.pptx"
-    },
-    {
-      "fileName": "song.mp3",
-      "fileType": "audio",
-      "fileSize": "4.5 MB",
-      "downloadUrl": "https://example.com/files/song.mp3"  
-    },
-    {
-      "fileName": "tutorial.mp4",
-      "fileType": "video",
-      "fileSize": "124 MB",
-      "downloadUrl": "https://example.com/files/tutorial.mp4"
-    },
-    {
-      "fileName": "report2.pdf",
-      "fileType": "document",
-      "fileSize": "1.2 MB",
-      "downloadUrl": "https://example.com/files/report2.pdf"
-    },
-    {
-      "fileName": "presentation2.pptx", 
-      "fileType": "document",
-      "fileSize": "6.7 MB",
-      "downloadUrl": "https://example.com/files/presentation2.pptx"
-    },
-    {
-      "fileName": "song2.mp3",
-      "fileType": "audio",
-      "fileSize": "3.1 MB",
-      "downloadUrl": "https://example.com/files/song2.mp3"   
-    },
-    {
-      "fileName": "video.avi", 
-      "fileType": "video",
-      "fileSize": "250 MB",
-      "downloadUrl": "https://example.com/files/video.avi"
-    },
-    {
-      "fileName": "ebook.pdf",
-      "fileType": "document",
-      "fileSize": "5 MB",
-      "downloadUrl": "https://example.com/files/ebook.pdf"
-    },
-    {
-      "fileName": "podcast.mp3",
-      "fileType": "audio",
-      "fileSize": "11 MB",
-      "downloadUrl": "https://example.com/files/podcast.mp3"
-    }
-  ]
-
-
-// get all files
-app.get("/", (req, res) => {
-    res.send(files)
-})
-
-
-// upload new file
-app.post("/", (req, res) => {
-    files.push(req.body)
-    res.send(201)
-})
