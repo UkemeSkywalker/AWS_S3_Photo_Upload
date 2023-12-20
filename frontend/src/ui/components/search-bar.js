ad36b8e96272a1d0221c487efceb4432ca2ce14d
Ukeme David Eseme <skywalkershadow9@gmail.com>
2023-12-20 14:08:48 +0000

    created UI components

---
+++ frontend/src/ui/components/search-bar.js
@@ -0,0 +1,9 @@
+const Searchbar = () => {
+    return(
+        <div className="search pt-5 ">
+              <input className="h-10 w-full rounded-full px-5" type="text" id="search-input"  placeholder="Search..." />
+            </div>
+    )
+}
+
+export default Searchbar
